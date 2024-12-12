0a74c1a619b3e4de1a64125541a76498f6041534
Anna <hisamutdinova.annna@gmail.com>
2024-12-12 11:06:38 +0100

    added routes

---
+++ app/routes/users.py
@@ -0,0 +1,46 @@
+from typing import Annotated
+
+from fastapi import APIRouter, HTTPException, Query
+from sqlmodel import select
+
+from app.db.database import SessionDep
+from app.db.models import User
+
+
+users_router = APIRouter()
+
+
+@users_router.post("/users/")
+def create_user(user: User, session: SessionDep) -> User:
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@users_router.get("/users/")
+def read_users(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[User]:
+    users = session.exec(select(User).offset(offset).limit(limit)).all()
+    return users
+
+
+@users_router.get("/users/{user_id}")
+def read_user(user_id: int, session: SessionDep) -> User:
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@users_router.delete("/users/{user_id}")
+def delete_user(user_id: int, session: SessionDep):
+    user = session.get(User, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    session.delete(user)
+    session.commit()
+    return {"ok": True}

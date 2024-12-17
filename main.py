b292cc81b3888c90093ecd7ca1e4111a0a660258
Anna <hisamutdinova.annna@gmail.com>
2024-12-17 16:14:50 +0100

    created heroku branch

---
+++ main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+from userapi.app.db.database import create_db_and_tables
+from userapi.app.routes.users import users_router
+from userapi.app.routes.healthcheck import healthcheck_router
+
+
+app = FastAPI()
+app.include_router(users_router, prefix='/api')
+app.include_router(healthcheck_router)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)

1ffda64a3d60b611c25c5f3bd37495778bbc88dd
Anna <hisamutdinova.annna@gmail.com>
2025-01-08 14:25:03 +0100

    added sqlite db

--- app/db/database.py
+++ app/db/database.py
@@ -13,7 +13,10 @@ db_url = (
     f"{settings.POSTGRES_DB}"
 )
 
-engine = create_engine(db_url)
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url)
 
 
 def create_db_and_tables():
